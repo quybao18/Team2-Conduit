e5f3da6dc4f5826d29973fc1ccf76eb86ddaee70
vanhhhhhhhhhhhh <163091992+vanhhhhhhhhhhhh@users.noreply.github.com>
2024-11-25 01:32:43 +0700

    Add profile, updateProfle, errorPage

--- src/src/App.js
+++ src/src/App.js
@@ -7,6 +7,9 @@ import Login from './pages/Login';
 import MyPost from './pages/MyPost';
 import NewPost from './pages/NewPost';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import UpdateProfile from './pages/UpdateProfile';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
@@ -17,7 +20,10 @@ function App() {
         <Route path='/register' element={<Register/>}/>
         <Route path='/' element={<Home/>}/>
         <Route path='/mypost/:mpid' element={<MyPost/>}/>
-        <Route path='/post/:pid' element={<DetailPost/>}/>        
+        <Route path='/post/:pid' element={<DetailPost/>}/>   
+        <Route path='/profile/:uid' element={<Profile/>}/>     
+        <Route path='/updateProfile/:uid' element={<UpdateProfile/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
       </Routes>
       </BrowserRouter>
     </div>
--- src/src/components/Header.js
+++ src/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Dropdown, Nav, Navbar } from 'react-bootstrap';
-import { FaSignInAlt, FaSignOutAlt, FaUserAlt } from 'react-icons/fa'; 
+import { FaSignInAlt, FaSignOutAlt, FaUserAlt } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import NewPost from '../pages/NewPost';
 
@@ -70,7 +70,7 @@ function Header() {
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                      <Dropdown.Item onClick={() => navigate('/profile')}>
+                      <Dropdown.Item onClick={() => navigate(`/profile/${user.id}`)}>
                         <FaUserAlt className="me-2" />
                         Profile
                       </Dropdown.Item>
